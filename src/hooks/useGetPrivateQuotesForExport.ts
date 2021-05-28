a3bc6ea35c6e02838f35eb95c251f6b4314a9b61
shuheishintani <shuheishintani.dev@gmail.com>
2021-05-29 00:25:45 +0900

    不要なインポートを削除

--- src/hooks/useGetPrivateQuotesForExport.ts
+++ src/hooks/useGetPrivateQuotesForExport.ts
@@ -1,20 +1,4 @@
-import { Book } from "../type/Book";
 import { useAxios } from "./useAxios";
-
-// type QuoteForExport struct {
-// 	ID   int           `gorm:"primary_key" json:"id"`
-// 	Text string        `json:"text"`
-// 	Page int           `json:"page"`
-// 	Book BookForExport `json:"book"`
-// 	Tags []string      `json:"tags"`
-// }
-
-// type BookForExport struct {
-// 	ISBN      string `json:"isbn"`
-// 	Title     string `json:"title"`
-// 	Author    string `json:"author"`
-// 	Publisher string `json:"publisher"`
-// }
 
 type BookForExport = {
   isbn: string;
